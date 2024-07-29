89f620ad8779477e5cd73232ff4fe11322bb52f4
sonyahash <85360714+sonyahash@users.noreply.github.com>
2024-07-29 14:39:21 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -17,3 +17,12 @@ terraform {
   }
   required_version = "~> 1.2"
 }
+
+# Specify the app in the project (one app_name per data block)
+data "hcp_vault_secrets_app" "aws_app" {
+  app_name = "terraform"
+  secret_name = "aws_region"
+}
+
+# Replace your existing secret references with
+# data.hcp-vault-secrets_app.aws_app.secret-name
